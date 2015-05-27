a055dda1fc44de33262dc11d350ddac75da41ddc
Charles Houston <charles.houston8@gmail.com>
2015-05-27 19:48:15 +0100

    Simplifying code and adding different particle colors.

--- js/experiments.js
+++ js/experiments.js
@@ -1,135 +1,19 @@
 var renderer, scene, camera;
 
-var pointcloud;
-
-var mouse = new THREE.Vector2();
-var intersection = null;
-
 var clock;
 
-var threshold = 0.1;
-var pointSize = 0.05;
-var width = 150;
-var length = 150;
+var particles, pGeom, pMat;
+
+var pointSize = 0.1;
+var NUMX = 10;
+var NUMY = 10;
+var WIDTH = 15;
+var LENGTH = 15;
+
 var rotateY = new THREE.Matrix4().makeRotationY( 0.005 );
 
 init();
 animate();
-
-function generatePointCloudGeometry( color, width, length ) {
-
-    var geometry = new THREE.BufferGeometry();
-    var numPoints = width*length;
-
-    var positions = new Float32Array( numPoints*3 );
-    var colors = new Float32Array( numPoints*3 );
-
-    var k = 0;
-
-    for( var i = 0; i < width; i++ ) {
-
-        for( var j = 0; j < length; j++ ) {
-
-            var u = i / width;
-            var v = j / length;
-            var x = u - 0.5;
-            var y = ( Math.cos( u * Math.PI * 8 ) + Math.sin( v * Math.PI * 8 ) ) / 20;
-            var z = v - 0.5;
-
-            positions[ 3 * k ] = x;
-            positions[ 3 * k + 1 ] = y;
-            positions[ 3 * k + 2 ] = z;
-
-            var intensity = ( y + 0.1 ) * 5;
-            colors[ 3 * k ] = color.r * intensity;
-            colors[ 3 * k + 1 ] = color.g * intensity;
-            colors[ 3 * k + 2 ] = color.b * intensity;
-
-            k++;
-
-        }
-
-    }
-
-    geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-    geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
-    geometry.computeBoundingBox();
-
-    return geometry;
-
-}
-
-function generatePointcloud( color, width, length ) {
-
-    var geometry = generatePointCloudGeometry( color, width, length );
-    
-    var material = new THREE.PointCloudMaterial( { size: pointSize, vertexColors: THREE.VertexColors } );
-    var pointcloud = new THREE.PointCloud( geometry, material );
-
-    return pointcloud;
-
-}
-
-function generateRegularPointcloud( color, width, length ) {
-
-    var geometry = new THREE.Geometry();
-    var numPoints = width * length;
-
-    var colors = [];
-
-    var k = 0;
-
-    for( var i = 0; i < width; i++ ) {
-
-        for( var j = 0; j < length; j++ ) {
-
-            var u = i / width;
-            var v = j / length;
-            var x = u - 0.5;
-            var y = ( Math.cos( u * Math.PI * 8 ) + Math.sin( v * Math.PI * 8 ) ) / 20;
-            var z = v - 0.5;
-            var v = new THREE.Vector3( x, y, z );
-
-            var intensity = ( y + 0.1 ) * 7;
-            colors[ 3 * k ] = color.r * intensity;
-            colors[ 3 * k + 1 ] = color.g * intensity;
-            colors[ 3 * k + 2 ] = color.b * intensity;
-
-            geometry.vertices.push( v );
-            colors[ k ] = ( color.clone().multiplyScalar( intensity ) );
-
-            k++;
-
-        }
-
-    }
-
-    geometry.colors = colors;
-    geometry.computeBoundingBox();
-
-    var material = new THREE.PointCloudMaterial( { size: pointSize, vertexColor: THREE.VertexColors } );
-    var pointcloud = new THREE.PointCloud( geometry, material );
-
-    return pointcloud;
-
-}
-
-function onWindowResize() {
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-}
-
-function onDocumentMouseMove( event ) {
-
-    event.preventDefault();
-
-    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-}
 
 function init() {
     
@@ -139,27 +23,50 @@ function init() {
 
     clock = new THREE.Clock();
 
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 20000 );
     camera.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 0, 20 ) );
     camera.applyMatrix( new THREE.Matrix4().makeRotationX( -0.5 ) );
 
-    //
-    
-    pointcloud = generatePointcloud( new THREE.Color( 0, 1, 0 ), width, length );
-    pointcloud.scale.set( 10, 10, 10 );
-    scene.add( pointcloud );
+    pGeom = new THREE.Geometry();
 
-    //
+    for( var i = 0; i < NUMX; i++ ) {
 
-    renderer = new THREE.WebGLRenderer();
+        for( var j = 0; j < NUMY; j++ ) {
+
+            var u = i / NUMX * WIDTH - WIDTH / 2;
+            var v = j / NUMY * LENGTH - LENGTH / 2;
+
+            var particle = new THREE.Vector3( u, 0, v );
+
+            var color = new THREE.Color();
+            color.setHSL( particle.y / 2, 1.0, 0.5 );
+
+            pGeom.vertices.push( particle );
+            pGeom.colors.push( color );
+
+        }
+
+    }
+
+    pMat = new THREE.PointCloudMaterial( { vertexColors: THREE.VertexColors, size: pointSize } );
+
+    particles = new THREE.PointCloud( pGeom, pMat );
+    scene.add( particles );
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
     container.appendChild( renderer.domElement );
 
-    //
-
     window.addEventListener( 'resize', onWindowResize, false );
-    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+}
+
+function onWindowResize() {
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
 }
 
